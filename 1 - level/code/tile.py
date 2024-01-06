21ac8d7c57a5c0c9d10f9d4aded168f617fcf660
FanncuS4n <fancus4n@gmail.com>
2024-01-06 20:16:30 -0300

    Camera movement and sprite overlapping

--- 1 - level/code/tile.py
+++ 1 - level/code/tile.py
@@ -6,3 +6,4 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('1 - level/graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-10) #Ajusta el tamaño de un rectángulo
