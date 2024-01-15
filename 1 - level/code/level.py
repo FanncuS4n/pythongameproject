0669e29e7e76699a44d231188eb2cd1ccf9ab9a8
FanncuS4n <fancus4n@gmail.com>
2024-01-15 20:25:09 -0300

    Enemy creation, movement and attacks

--- 1 - level/code/level.py
+++ 1 - level/code/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
     def __init__(self):
@@ -30,10 +31,11 @@ class Level:
         layouts =  {
             'boundary': import_csv_layout('1 - level/map/map_FloorBlocks.csv'),
             'grass': import_csv_layout('1 - level/map/map_Grass.csv'),
-            'object': import_csv_layout('5 - level graphics/map/map_Objects.csv')
+            'object': import_csv_layout('5 - level graphics/map/map_Objects.csv'),
+            'entities': import_csv_layout('5 - level graphics/map/map_Entities.csv')
         }
         graphics = {
-            'grass': import_folder('1 - level\graphics\grass'),
+            'grass': import_folder('1 - level/graphics/grass'),
             'objects': import_folder('1 - level/graphics/objects')
         }
         
@@ -45,6 +47,7 @@ class Level:
                         y = row_index * TILESIZE
                         if style == 'boundary':
                             Tile((x, y), [self.obstacle_sprites], 'invisible')
+                            
                         if style == 'grass':
                             random_grass_img = choice(graphics['grass'])
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_img)
@@ -52,15 +55,24 @@ class Level:
                         if style == 'object':
                             object_surf = graphics['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', object_surf)
-                        
-        self.player = Player(
-            (2000,1430), 
-            [self.visible_sprites], 
-            self.obstacle_sprites, 
-            self.create_attack, 
-            self.destroy_attack, 
-            self.create_magic)
-       
+                            
+                        if style == 'entities':
+                            if col == '394':
+                                self.player = Player(
+                                        (x,y), 
+                                        [self.visible_sprites], 
+                                        self.obstacle_sprites, 
+                                        self.create_attack, 
+                                        self.destroy_attack, 
+                                        self.create_magic)
+                            else:
+                                if col == '390': monster_name = 'bamboo'
+                                elif col == '391': monster_name = 'spirit'
+                                elif col == '392': monster_name = 'raccoon'
+                                else: monster_name = 'squid'
+                                    
+                                Enemy(monster_name, (x,y), [self.visible_sprites], self.obstacle_sprites)
+                                
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites])
         
@@ -76,6 +88,7 @@ class Level:
         #Update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
         self.ui.display(self.player)
        
 class YSortCameraGroup(pygame.sprite.Group):
@@ -106,3 +119,8 @@ class YSortCameraGroup(pygame.sprite.Group):
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+            
+    def enemy_update(self, player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
--- 1 - level/code/settings.py
+++ 1 - level/code/settings.py
@@ -38,8 +38,15 @@ magic_data = {
   'heal': {'strenght': 20, 'cost': 10, 'graphic': '1 - level/graphics/particles/heal/heal.png'}
 }
 
+#ENEMY 
+monster_data = {
+  'squid': {'health': 100, 'exp': 100, 'damage': 20, 'attack_type': 'slash', 'attack_sound': '10 - Enemies/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+  'raccoon': {'health': 300, 'exp': 250, 'damage': 40, 'attack_type': 'claw', 'attack_sound': '10 - Enemies/audio/attack/claw.wav', 'speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+  'spirit': {'health': 100, 'exp': 110, 'damage': 8, 'attack_type': 'thunder', 'attack_sound': '10 - Enemies/audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+  'bamboo': {'health': 700, 'exp': 120, 'damage': 6, 'attack_type': 'leaf_attack', 'attack_sound': '10 - Enemies/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}
+}
+
 # # OLD MAP DISPLAY
-# # p se encuentra en XY(128, 128)
 # WORLD_MAP = [
 # ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
 # ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
